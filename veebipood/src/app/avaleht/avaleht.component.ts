9171e1b3970344109dad955363689158d0f2a85b
mihkelvaher <mihkel.vaher@industry62.com>
2022-02-17 14:39:07 +0200

    Muutujatetüübid, {{ }} string interpolation, ngIf - direktiiv, (click) - event binding

--- veebipood/src/app/avaleht/avaleht.component.ts
+++ veebipood/src/app/avaleht/avaleht.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AvalehtComponent implements OnInit {
 
-  constructor() { }
+  s6na = "312"; // vasakul pool võrdusmärgist on muutuja, paremal pool selle väärtus
+  number = 312; // 1. numbriga on võimalik teha tehteid ---- number + number = loogiline number
+                // sõnaline number + sõnaline number = kokkuliitmine nagu sõnadega
+                // 5 + 8 = 13
+                // "5" + "8" = "58"   --- "raud" + "tee"
+  kahendV22rtus = false; // true/false --- "y" / "n" --- 0 / 1  --- kahevahel: "p"   ---- 2
+          // checkbox - nõustunud/mittenõustunud tingimustega
+          // täisealine / mitte
+  
+  massiiv = ["dasdas","asdasd"];
+  list = [13,13,3124,123];
+  arrray = [true,false,true];
+
+  constructor() { 
+    console.log("AvalehtComponent constructor käivitub");
+  }
 
   ngOnInit(): void {
+    console.log("AvalehtComponent ngOnInit käivitub");
+  }
+
+  funktsioon() {
+    console.log("funktsioon käivitub");
+    this.kahendV22rtus = !this.kahendV22rtus;
+    this.number = this.number + 100;
   }
 
 }
--- veebipood/src/app/ostukorv/ostukorv.component.ts
+++ veebipood/src/app/ostukorv/ostukorv.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-ostukorv',
-  templateUrl: './ostukorv.component.html',
-  styleUrls: ['./ostukorv.component.css']
-})
-export class OstukorvComponent implements OnInit {
+@Component({ // imporditakse node_modules seest ja saab kõik componendile vajalikud omadused
+  selector: 'app-ostukorv', // võtan selectori abil componendi kasutusele <app-ostukorv></app-ostukorv>
+  templateUrl: './ostukorv.component.html', // seotakse HTML fail, mida on iga kord täpselt 1,
+      // ka mitut .ts faili ei saa ühte HTMLi siduda; alati on 1===1 -le seos
+  styleUrls: ['./ostukorv.component.css'] // siit seotakse CSS fail, mida võib ka olla mitu tk
+}) // [] - tähistavad massiivi-listi-array   
+// {} - tähistavad objekti - võti-väärtus paaride kogumik või funktsiooni/classi algust ja lõppu
+//   () - parameetrite vastu võtmiseks, kui seda välja kutsun, siis annan midagi kaasa
+export class OstukorvComponent implements OnInit { // export - et oleks võimalik importida
+  // nt importisime app-routing failis luues seoseid path:"" ja component:C vahel
+  // class - tähistab funktsioonide ja muutujate kogumit
+  // OstukorvComponent - tekib genereerides (ng generate component ostukorv)
+  // implements OnInit --- OnInit on imporditud node_modules kaustast ja annab mingit lisaomadust
+  //      et ei saaks kustutada ega muuta ngOnInit funktsiooni
 
-  constructor() { }
+  constructor() { 
+    console.log("OstukorvComponent constructor käivitub");
+  } // erinevate koodilõikude ühendamiseks node_modules kaustast
+  // kui luuakse Component, siis ühendatakse mingid teatud failid mis on siia sisse pandud
+  // loomise moment on kui URL peale satutakse
 
-  ngOnInit(): void {
+  ngOnInit(): void { // ngOnInit - käimaminemise funktsioon
+    // käimaminemise moment on kui URL peale satutakse
+    // funktsioon, mis on loogelisest sulust loogelise suluni
+    // näen kust ta algab ja lõppeb kui vajutan ühe loogelise sulu peale ja tekib mõlema
+          // ümber kast
+    console.log("OstukorvComponent ngOnInit käivitub");
   }
 
-}
+} // kõikidele loogeliste, kandilistele ja ümaratele sulgudele on vaja programmeerimsies
+// paarilist. ei tohi olla nii, et läheb sulg lahti aga ei lähe kinni {} () []
+// HTMLs: <div></div> <button></button>  erijuhud: <img />  <input />
